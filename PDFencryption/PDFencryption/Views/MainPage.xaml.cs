9d904e9e013f7e5142954101cc70e2ed551f5560
Philip Ruggirello <pjruggirello@gmail.com>
2020-11-02 21:49:58 -0500

    Basic Bones

---
+++ PDFencryption/PDFencryption/Views/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using PDFencryption.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDFencryption.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async void ScanClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ScanScreenPage());
+            
+        }
+    }
+}
---
+++ PDFencryption/PDFencryption/Views/ScanScreenPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using PDFencryption.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDFencryption.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScanScreenPage : ContentPage
+    {
+        public ScanScreenPage()
+        {
+            InitializeComponent();
+        }
+        public async void BarcodeClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Results());
+
+        }
+    }
+}
