6f3eb68a0991dba6dbc77bde98a60801c689bba9
John Shashaty <john.shashaty@fairfield.edu>
2020-12-01 20:12:03 -0500

    models

---
+++ PDFencryption/PDFencryption/Models/Firebase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+
+namespace PDFencryption.Models
+
+{
+    
+    class Firebase
+    {
+        FirebaseClient firebase = new FirebaseClient("https://sd-barcode-security.firebaseio.com/");
+        public string key = { get; set; } //"8f88bf83d115c4152ef46f8b1c7347f0017751c4333a05c1abc8eb319f760d4601e9dde106b30a87893fa4cc8a163aa33dc1cd8408b14eacdcd311291fe994db";
+        public string text = { get; set; }
+
+
+
+    }
+}
