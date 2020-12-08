0905821d624b20ca6c13edf2ff9852a0b6dd76b1
Guillem Pérez <guillempp@users.noreply.github.com>
2020-12-08 15:47:05 -0500

    Decryption Finally might work

--- PDFencryption/PDFencryption/Views/ResultsPage.xaml.cs
+++ PDFencryption/PDFencryption/Views/ResultsPage.xaml.cs
@@ -19,18 +19,17 @@ namespace PDFencryption.Views
             InitializeComponent();
 
             MainLabel.Text = result;
-            var dkey = key;
+            byte[] dkey = Convert.FromBase64String(key);
+            byte[] cipherText = Convert.FromBase64String(result);
 
 
 
         }
 
 
-     
-
         async void Decrypt_Button_Clicked(object sender, System.EventArgs e)
         {
-             ResultsPage.DecryptString(dkey, result);
+             ResultsPage.DecryptStringFromBytes_Aes(dkey, cipherText);
 
 
         }
@@ -44,6 +43,49 @@ namespace PDFencryption.Views
             await Navigation.PushAsync(new ScanScreenPage());
         }
 
+        static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (RijndaelManaged aesAlg = new RijndaelManaged())
+            {
+                aesAlg.BlockSize = 128;
+                aesAlg.KeySize = 256;
+                aesAlg.Padding = PaddingMode.PKCS7;
+                aesAlg.Mode = CipherMode.ECB;
+                aesAlg.Key = Key;
+                //aesAlg.Padding = PaddingMode.None;
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, null);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return plaintext;
+        }
 
         
 
